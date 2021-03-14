63d5fd2a8b8a8cb97e04a71c1661eb0ed8fd94af
Sonja Goerlich <so.goerlich@gmail.com>
2021-03-14 17:13:06 +0100

    inital commit

---
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -0,0 +1,24 @@
+package com.udacity.jwdnd.c1.review;
+
+import com.udacity.jwdnd.c1.review.service.MessageService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+
+
+
+
+
+}
